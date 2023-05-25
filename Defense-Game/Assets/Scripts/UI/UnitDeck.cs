d78f6412660ad46fe19aabc1cc08ed6c51c72ce3
vlaud <whehdgus1207@naver.com>
2023-05-25 12:24:08 +0900

    싱글 더블 클릭 구분 기능

--- Defense-Game/Assets/Scripts/UI/UnitDeck.cs
+++ Defense-Game/Assets/Scripts/UI/UnitDeck.cs
@@ -1,9 +1,73 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
+using System.Collections;
 
-public class UnitDeck : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class UnitDeck : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public float timeLimit = 0.25f;
+    public PointerEventData.InputButton button;
+
+    [System.Serializable]
+    public class OnSingleClick : UnityEvent { };
+    public OnSingleClick onSingleClick;
+
+    [System.Serializable]
+    public class OnDoubleClick : UnityEvent { };
+    public OnDoubleClick onDoubleClick;
+
+    private int clickCount;
+    private float firstClickTime;
+    private float currentTime;
+
+    private UnitDeck()
+    {
+        clickCount = 0;
+    }
+
+    public void onClick(BaseEventData data)
+    {
+
+        PointerEventData pointerData = data as PointerEventData;
+
+        if (this.button != pointerData.button)
+        {
+            return;
+        }
+
+        this.clickCount++;
+
+        if (this.clickCount == 1)
+        {
+            firstClickTime = pointerData.clickTime;
+            currentTime = firstClickTime;
+            StartCoroutine(ClickRoutine());
+        }
+    }
+
+    private IEnumerator ClickRoutine()
+    {
+
+        while (clickCount != 0)
+        {
+            yield return new WaitForEndOfFrame();
+
+            currentTime += Time.deltaTime;
+
+            if (currentTime >= firstClickTime + timeLimit)
+            {
+                if (clickCount == 1)
+                {
+                    onSingleClick.Invoke();
+                }
+                else
+                {
+                    onDoubleClick.Invoke();
+                }
+                clickCount = 0;
+            }
+        }
+    }
     private RectTransform rect;
     private Transform prevParent;
     private int sortIndex;
@@ -28,13 +92,6 @@ public class UnitDeck : MonoBehaviour, IPointerClickHandler, IBeginDragHandler, 
         rect = GetComponent<RectTransform>();
         prevParent = transform.parent;
         sortIndex = transform.GetSiblingIndex();
-    }
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (eventData.clickCount == 1)
-        {
-            OnAction();
-        }
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
