56d3b0a841d86ccb29c262f816a1c3bf71105deb
vlaud <whehdgus1207@naver.com>
2023-06-19 10:35:58 +0900

    캐릭터 카드 3종 추가

--- Defense-Game/Assets/Scripts/UI/UnitStatus.cs
+++ Defense-Game/Assets/Scripts/UI/UnitStatus.cs
@@ -2,22 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum UnitPosition
+{
+    Deal,
+    Tank,
+    Heal
+}
 [CreateAssetMenu(fileName = "UnitStatusData", menuName = "Resource Data/Unit Data")]
 public class UnitStatus : ScriptableObject
 {
     [SerializeField]
     private Sprite unitSprite;
     [SerializeField]
+    private string unitName;
+    [SerializeField]
+    private string unitDescription;
+    [SerializeField]
+    private UnitPosition unitPosition;
+    [SerializeField]
     private float hp;
-    [SerializeField]
-    private float ap;
-    [SerializeField]
-    private float moveSpeed;
-    [SerializeField]
-    private float attackSpeed;
     [SerializeField]
     private float dp;
     [SerializeField]
+    private float ap;
+    [SerializeField]
+    private float attackSpeed;
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
     private float attackRange;
-
 }
